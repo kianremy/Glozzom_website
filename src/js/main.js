7976f30f7828cfa7e31c2b364203652926e5cbd1
kianremy <ultanjim75@gmail.com>
2024-09-25 06:19:44 -0700

    glozzom_web

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$('.carousel').carousel({
+  interval : 4000,
+  pause    : 'hover',
+  wrap     : true,
+  keyboard : true
+})
+
+$('.slider').slick({
+  infinite      : true,
+  slideToShow   : 1,
+  slideToScroll : 1
+})
